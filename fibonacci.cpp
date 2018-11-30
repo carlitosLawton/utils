01e5ecee806d3559da5817074d7e2cd125c1eae2
ichicar <fexichikawa@gmail.com>
2018-11-29 20:03:14 -0500

    Adding fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int fib(const int i)
+{
+  if(i == 0) return 0;
+  if(i == 1) return 1;
+  return fib(i-1) + fib(i-2);
+}  
+
+int main()
+{
+  std::cout << fib(10) << '\n';
+}
